875ae4abdd4c6bd3aadf409333ddc6114d5895ea
Chloé R <clositi@live.fr>
2021-11-15 02:18:16 +0100

    test done for handleSubmit() in newBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,16 +1,54 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import BillsUI from "../views/BillsUI.js"
 import firebase from "../__mocks__/firebase.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES, ROUTES_PATH } from "../constants/routes"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+    describe("When I upload a new file with the right format", () => {
+      test("Then the image should be in the file handler", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
+        //to-do write assertion
+      })
+    })
+    describe("When I upload a new file with a bad format", () => {
+      test("Then it should display an error message", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
+        //to-do write assertion
+      })
+    })
+    describe("When I submit a new bill with all the correct informations", () => {
+      test("Then a new bill should be created and I should be sent to Bills Page", () => {
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        const html = NewBillUI();
+        document.body.innerHTML = html;
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const firestore = null
+        const newBill = new NewBill({
+          document, onNavigate, firestore, localStorage: window.localStorage
+        })
+
+        const form = screen.getByTestId('form-new-bill')
+        const handleSubmit = jest.fn(newBill.handleSubmit)
+        form.addEventListener('submit', handleSubmit)
+        fireEvent.submit(form)
+        expect(handleSubmit).toHaveBeenCalled()
+
+        const titleBills = screen.getAllByText('Mes notes de frais')
+        expect(titleBills).toBeTruthy()
+
+      })
     })
   })
 })
